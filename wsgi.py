36d95b9ab67727269d1cf1ab9f5602c809448862
Roman Mikhailov <154780185+ccrayp@users.noreply.github.com>
2025-09-18 08:39:39 +0300

    remove auto-alive

--- wsgi.py
+++ wsgi.py
@@ -5,18 +5,5 @@ import time
 
 application = init_app()
 
-def keep_alive():
-    while True:
-        try:
-            requests.get("https://ccrayp.onrender.com/api/post/list")
-        except Exception as e:
-            print("База данных уснула")
-        time.sleep(600)
-
-if __name__ != "__main__":
-    t = threading.Thread(target=keep_alive)
-    t.daemon = True
-    t.start()
-
 if __name__ == '__main__':
     application.run(port=8000)
