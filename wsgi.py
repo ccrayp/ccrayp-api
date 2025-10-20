f1ab597e1b0199174399ce14b169ce4869a01141
Roman Mikhailov <154780185+ccrayp@users.noreply.github.com>
2025-10-20 20:00:31 +0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -5,18 +5,18 @@ import time
 
 application = init_app()
 
-def keep_alive():
-    while True:
-        try:
-            requests.get("https://ccrayp.onrender.com/api/post/list")
-        except Exception as e:
-            print("База данных уснула")
-        time.sleep(299)
+# def keep_alive():
+#     while True:
+#         try:
+#             requests.get("https://ccrayp.onrender.com/api/post/list")
+#         except Exception as e:
+#             print("База данных уснула")
+#         time.sleep(299)
 
-if __name__ != "__main__":
-    t = threading.Thread(target=keep_alive)
-    t.daemon = True
-    t.start()
+# if __name__ != "__main__":
+#     t = threading.Thread(target=keep_alive)
+#     t.daemon = True
+#     t.start()
 
 if __name__ == '__main__':
     application.run(port=8000)
